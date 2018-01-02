a15f5abf3294fc1b22d2e06318365d099b1f9c18
myreage <brassenslebg@gmail.com>
2018-01-02 18:19:39 +0100

    fuff

--- graph.ml
+++ graph.ml
@@ -4,25 +4,28 @@ module type OrderedType = sig
     type t
     val compare : t -> t -> int
   end
-  
+
   (* Signature des graphes *)
   module type MyGraph = sig
     type node
     module NodeSet : Set.S with type elt = node*int
     module NodeMap : Map.S with type key = node
     type graph
-  
+
     val empty : graph
     val is_empty : graph -> bool
-  
+
     val add_node : node -> graph -> graph
-  
+
     val add_edge : node -> node -> int -> graph -> graph
-  
+
     val fold_node : (node -> 'a -> 'a) -> graph -> 'a -> 'a
     val fold_edge : (node -> node*int -> 'a -> 'a) -> graph -> 'a -> 'a
-  
+
     val succs : node -> graph -> NodeSet.t
+
+		val distance : node -> node -> graph -> int
+
   end
 
 (* Foncteur pour faire des graphes *)
@@ -45,29 +48,39 @@ module Make (X : OrderedType) : (MyGraph with type node = X.t) = struct
         if NodeMap.mem n g then g
         else NodeMap.add n NodeSet.empty g
 
-    let add_edge src dst value g =
-        let src_succs =
-            try NodeMap.find src g
-            with Not_found -> NodeSet.empty in
-        let src_succs_with_dist = NodeSet.add (dst,value) src_succs in
-        let g2 = NodeMap.add src src_succs_with_dist g in
-        add_node dst g2
+		let find n g = NodeMap.find n g
 
-    let remove_edge src dst g =
-        try
-            let src_succs = (NodeMap.find src g) in
-            let src_succs_without_dist = NodeSet.remove dst src_succs in
-            NodeMap.add src src_succs_without_dist g
-        with Not_found -> g
+    let add_edge a b value g =
+        let a_succs =
+            try NodeMap.find a g
+            with Not_found -> NodeSet.empty in
+					let b_succs =
+		        try NodeMap.find b g
+		        with Not_found -> NodeSet.empty in
+        let a_succs_with_dist = NodeSet.add (b,value) a_succs in
+				let b_succs_with_dist = NodeSet.add (a,value) b_succs in
+        let g = NodeMap.add a a_succs_with_dist g in
+				let g = NodeMap.add b b_succs_with_dist g in
+        let g = add_node b g in
+				add_node b g
 
     let fold_node f g v0 = NodeMap.fold (fun n n_succs acc -> f n acc) g v0
 
     let fold_edge f g v0 =
         NodeMap.fold (
-            fun src src_succs acc -> NodeSet.fold (fun dst acc_src -> f src dst acc_src) src_succs acc
+            fun a a_succs acc -> NodeSet.fold (fun b acc_a -> f a b acc_a) a_succs acc
         ) g v0
 
-    let succs n g = NodeMap.find n g;;
+    let succs n g = NodeMap.find n g
+
+		let distance a b g =
+			let rec aux l x = match l with
+				|[] -> 0
+				|a::b -> let (h,j) = a in if h=x then j else aux b x
+				in
+			let x = NodeMap.find a g in
+				let elts = NodeSet.elements x in
+					aux elts b
 end
 
 module Int : (OrderedType with type t = int) = struct
@@ -77,13 +90,6 @@ end
 
 module MyStringGraph = Make(String);; (* Graphe dont les noeuds sont des String, avec arcs valués *)
 
-let test = MyStringGraph.add_edge "n1" "n2" 3 MyStringGraph.empty;;
-let test = MyStringGraph.add_edge "n2" "n4" 5 test;;
-let test = MyStringGraph.add_edge "n1" "n4" 7 test;;
-
-print_string "Nodes : "; print_string (MyStringGraph.fold_node (fun n acc -> (n^" - "^acc)) test "");;
-print_string "\nEdges : "; print_string (MyStringGraph.fold_edge (fun src (x,y) acc_src -> (acc_src^" - "^x^" #Value : "^(string_of_int y)^"\n")) test "");;
-
 let (liste, (init,last)) = Analyse.analyse_file_1 "1.txt";;
 
 let rec createGraph l g = match l with
@@ -91,3 +97,10 @@ let rec createGraph l g = match l with
 	|a::b ->	let (x,y,z) = a in
 						let g2 =  MyStringGraph.add_edge x y z g in
 						createGraph b g2;;
+
+let g = createGraph liste MyStringGraph.empty;;
+print_string "Nodes : "; print_string (MyStringGraph.fold_node (fun n acc -> (n^" - "^acc)) g "");;
+print_string "Edges : \n"; print_string (MyStringGraph.fold_edge (fun a (d1,d2) acc_a -> (acc_a^a^" - "^d1^" #"^(string_of_int d2)^"\n")) g "");;
+
+
+(* DJIKSTRA*)
