75a9b0ca7edd43cd98b20616842dffcb6d25a0fe
myreage <brassenslebg@gmail.com>
2018-01-03 18:54:53 +0100

    marche pas jpp

--- graph.ml
+++ graph.ml
@@ -25,6 +25,9 @@ module type OrderedType = sig
     val succs : node -> graph -> NodeSet.t
 
 		val distance : node -> node -> graph -> int
+		val bindings : graph -> (NodeMap.key * NodeSet.t) list
+		val voisins : node -> node list -> node list
+		val is_node_in_set : node -> NodeSet.t -> bool
 
   end
 
@@ -72,6 +75,7 @@ module Make (X : OrderedType) : (MyGraph with type node = X.t) = struct
         ) g v0
 
     let succs n g = NodeMap.find n g
+		let bindings g = NodeMap.bindings g
 
 		let distance a b g =
 			let rec aux l x = match l with
@@ -81,6 +85,14 @@ module Make (X : OrderedType) : (MyGraph with type node = X.t) = struct
 			let x = NodeMap.find a g in
 				let elts = NodeSet.elements x in
 					aux elts b
+
+			let rec is_node_in_set n set = match set with
+				|[] -> 0
+				|(x,y)::t -> if x=n then 1 else is_node_in_set n t;;
+
+			let rec voisins a nodes_list = match nodes_list with
+				|[]-> []
+				|(x,y)::t -> if is_node_in_set a y then (x,y)::(voisins a t);;
 end
 
 module Int : (OrderedType with type t = int) = struct
@@ -88,7 +100,7 @@ module Int : (OrderedType with type t = int) = struct
     let compare = compare
 end
 
-module MyStringGraph = Make(String);; (* Graphe dont les noeuds sont des String, avec arcs valués *)
+module MyStringGraph = Make(String);; (* Graphe dont les noeuds sont des String, avec arcs valués et sans direction *)
 
 let (liste, (init,last)) = Analyse.analyse_file_1 "1.txt";;
 
@@ -104,3 +116,59 @@ print_string "Edges : \n"; print_string (MyStringGraph.fold_edge (fun a (d1,d2) 
 
 
 (* DJIKSTRA*)
+print_string (string_of_int (MyStringGraph.distance "n2" "n4" g));;
+
+let rec init_distances nodes start = match nodes with
+		|[] -> []
+		|(x,y)::t -> 	if x=start then 0::(init_distances t start)
+									else 999999::(init_distances t start);;
+
+let rec find_index_distances x nodes i = match nodes with
+	| [] -> 0
+	| (a,b)::t -> if x = b then i else find_index_distances x t i+1;;
+
+let rec find_min nodes_dijkstra nodes distances mini sommet i = match nodes with
+	|[] -> sommet
+	|(x,y)::t -> if ((distances.[i] < mini) && not(List.mem (x,y) nodes_dijkstra))
+	 						 then (find_min nodes_dijkstra t distances distances.[i] x i+1)
+							 else (find_min nodes_dijkstra t distances mini sommet i+1);;
+
+let update_distances a b (distances : int list) nodes g predecereurs =
+	let ia = find_index_distances a nodes 0 in
+	let ib = find_index_distances b nodes 0 in
+	let poidsab = MyStringGraph.distance a b g in
+	let pred = predecereurs in
+	let new_dist = distances in
+	if (List.nth distances ib) > ((List.nth distances ia) + poidsab) then
+	new_dist.[ib] = distances.[ia] + poidsab;
+	pred.[ib] = pred.[ia];
+	(new_dist,pred);;
+
+let b = MyStringGraph.bindings g;;
+let distances = init_distances (MyStringGraph.bindings g) "n1";;
+
+let diff l1 l2 = List.filter (fun x -> not (List.mem x l2)) l1
+
+let dijkstra g start =
+
+	let rec aux nodes distances = match nodes with (*P résultat*)
+	|[] -> distances
+	|(x,y)::t ->	let Q = List.filter (fun x -> x=)
+	 							let min = find_min Q nodes distances 99999 "" 0
+		in
+
+		let rec aux2 voisins = match voisins with
+		|[] -> distances
+		|(a,b)::t -> aux2 t update_distances min (a,b) distances nodes g predecereurs
+		in
+		let distances2 = aux2 MyStringGraph.voisins min nodes (* renvoie les nouvelles distances*)
+		in
+
+		aux
+
+
+
+	let predecereurs = [] in
+	let nodes = MyStringGraph.bindings g in
+	let distances = init_distances nodes start in
+	let res = aux [] nodes nodes;;
